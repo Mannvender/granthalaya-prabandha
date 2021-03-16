852bdd550575f8804b8ed5e9f8f7e5b172337766
mannvenders <mannvenders@dewsolutions.in>
2021-03-16 20:40:03 +0530

    minor cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 // library imports
-import React from 'react'
-import {BrowserRouter as Router, Switch} from 'react-router-dom'
-import {ThemeProvider} from 'styled-components'
+import React from "react"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
-import './App.css'
-import theme from 'theme'
-import routes from 'routes'
-import PublicRoute from 'routes/Public'
-import PublicOnlyRoute from 'routes/PublicOnly'
-import PrivateRoute from 'routes/Private'
+import "./App.css"
+import theme from "theme"
+import routes from "routes"
+import PublicRoute from "routes/Public"
+import PublicOnlyRoute from "routes/PublicOnly"
+import PrivateRoute from "routes/Private"
 
 function App() {
   return (
     <ThemeProvider
       theme={{
-        color: theme.color.light,
+        color: theme.color.dark,
         size: theme.size.medium,
         edgeSize: theme.edgeSize,
       }}
     >
       <Router>
         <Switch>
-          {routes.map(route => {
+          {routes.map((route) => {
             if (route.private) {
               return <PrivateRoute key={route.path} {...route} />
             } else if (route.publicOnly) {
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 const theme = {
   color: {
-    light: {'dark-1': 'red'},
-    dark: {'dark-1': 'blue'},
+    light: {'dark-1': '#fff'},
+    dark: {'dark-1': '#1e1e1e'},
   },
   size: {
     // by default size choosen by user is medium
