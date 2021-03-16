8249584ce1b66b5abac3c5d9ccf790030f0a4b76
mannvenders <mannvenders@dewsolutions.in>
2021-03-16 20:40:16 +0530

    updated box component

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,24 +1,51 @@
-import * as React from 'react'
-import styled from 'styled-components'
+import * as React from "react"
+import styled, { css } from "styled-components"
 
 const StyledDiv = styled.div<{
   height: string
   bgColor: string
+  justifyContent: string
+  alignItems: string
 }>`
   display: flex;
   flex-direction: column;
-  height: ${({theme, height}) => theme.size[height] || height};
-  background-color: ${({theme, bgColor}) => theme.color[bgColor] || bgColor};
+  height: ${({ theme, height }) => theme.size[height] || height};
+  background-color: ${({ theme, bgColor }) => theme.color[bgColor] || bgColor};
+  ${({ justifyContent }) =>
+    justifyContent &&
+    css`
+      justify-content: ${justifyContent};
+    `}
+  ${({ alignItems }) =>
+    alignItems &&
+    css`
+      align-items: ${alignItems};
+    `}
 `
 
 interface Props {
   height?: string
   bgColor?: string
   children?: any
+  justifyContent?: string
+  alignItems?: string
 }
-const Box = ({children, height = '', bgColor = '', ...rest}: Props) => {
+const Box = ({
+  alignItems = "",
+  bgColor = "",
+  children,
+  justifyContent = "",
+  height = "",
+  ...rest
+}: Props) => {
   return (
-    <StyledDiv height={height} bgColor={bgColor} {...rest}>
+    <StyledDiv
+      height={height}
+      bgColor={bgColor}
+      alignItems={alignItems}
+      justifyContent={justifyContent}
+      {...rest}
+    >
       {children}
     </StyledDiv>
   )
