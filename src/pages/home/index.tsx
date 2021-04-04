77ee2fe0c98bb400c1121531a79034acabbaf9b0
mannvender <mannvender001@gmail.com>
2021-04-04 10:42:35 +0530

    moved utility func

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,13 @@
 import * as React from 'react'
 import { useRef, useCallback, useState, useEffect } from 'react'
-import Webcam from 'react-webcam'
-import Box from 'components/Box'
 import { Rekognition } from '@aws-sdk/client-rekognition'
 import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity'
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-provider-cognito-identity'
+import Webcam from 'react-webcam'
+
+import Box from 'components/Box'
+import convertDataURIToBinary from 'utils/base64-to-uint8array'
 const { REACT_APP_AWS_REGION, REACT_APP_AWS_IDENTITY_POOL_ID } = process.env
-const BASE64_MARKER = ';base64,'
-
-function convertDataURIToBinary(dataURI: string) {
-  const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length
-  const base64 = dataURI.substring(base64Index)
-  const raw = window.atob(base64)
-  const rawLength = raw.length
-  const array = new Uint8Array(new ArrayBuffer(rawLength))
-
-  for (let i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i)
-  }
-  return array
-}
 
 const Home = () => {
   const webcamRef = useRef<Webcam>(null!)
