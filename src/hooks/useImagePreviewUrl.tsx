9eef241980214133691fb305fd7b8126c63cea5b
mannvenders <mannvenders@dewsolutions.in>
2021-04-06 11:11:04 +0530

    added useImagePreviewUrl hook

---
+++ src/hooks/useImagePreviewUrl.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+interface Props {
+  blob: Blob
+}
+const useImagePreviewUrl = ({ blob }: Props) => {
+  const [imagePreviewUrl, setPreview] = useState<any>('')
+  useEffect(() => {
+    if (blob) {
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        const previewUrl = reader.result || ''
+        setPreview(previewUrl)
+      }
+      reader.readAsDataURL(blob)
+    }
+  }, [blob])
+
+  return imagePreviewUrl
+}
+
+export default useImagePreviewUrl
