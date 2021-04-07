1cb9f54eee1c97c746dfe90f663bf59607f012b5
mannvender <mannvender001@gmail.com>
2021-04-07 19:25:43 +0530

    Updates

---
+++ precache-manifest.099b14fecd28f2abc9d09682a446838f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e2ee7663017c8413875d16005a992f",
+    "url": "/granthalaya-prabandha/register/index.html"
+  },
+  {
+    "revision": "5232252f2d092be4254d",
+    "url": "/granthalaya-prabandha/register/static/css/main.19723893.chunk.css"
+  },
+  {
+    "revision": "9f498678787aa22abe73",
+    "url": "/granthalaya-prabandha/register/static/js/2.7f122864.chunk.js"
+  },
+  {
+    "revision": "b4ea465c4ce2f5521b7c5ded26858627",
+    "url": "/granthalaya-prabandha/register/static/js/2.7f122864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5232252f2d092be4254d",
+    "url": "/granthalaya-prabandha/register/static/js/main.ac37daf0.chunk.js"
+  },
+  {
+    "revision": "d9e9cc71d2c3a6220da5",
+    "url": "/granthalaya-prabandha/register/static/js/runtime-main.8ac245b1.js"
+  }
+]);
