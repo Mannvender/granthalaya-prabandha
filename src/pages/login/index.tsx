8db9489a7d0db060f8040b18aa5037d0a05b2048
mannvender <mannvender001@gmail.com>
2021-04-07 17:53:32 +0530

    added loader in login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,25 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLoading } from '@agney/react-loading'
+import { useIndexedDB } from 'react-indexed-db'
 
 import Box from 'components/Box'
 import Webcam from 'components/Webcam'
+import LoaderContainer from 'components/LoaderContainer'
 import useSearchFaces from 'hooks/useSearchFaces'
 
 const Login = () => {
   const [base64Image, setImage] = useState('')
-  const { isFetching, isSuccess, error } = useSearchFaces({ base64Image })
+  const [studentInfo, setStudentInfo] = useState({})
+  const { faceMatches, isFetching, isSuccess, error } = useSearchFaces({
+    base64Image,
+  })
+  const { containerProps, indicatorEl } = useLoading({
+    loading: isFetching && !isSuccess,
+  })
+  const { getByID } = useIndexedDB('students')
   // eslint-disable-next-line no-console
   console.log(
+    faceMatches,
     isFetching,
     isSuccess,
     error,
     '------isFetching, isSuccess, error-----',
   )
 
+  useEffect(() => {
+    if (!faceMatches.length) return
+
+    const faceMatch = faceMatches[0]
+    if (faceMatch.Similarity && faceMatch.Similarity > 80) {
+      const indexedDbId = faceMatch.Face?.ExternalImageId
+      if (indexedDbId)
+        getByID(indexedDbId)
+          .then((studentFromDB) => {
+            delete studentFromDB.image
+            setStudentInfo(studentFromDB)
+          })
+          .catch(console.error)
+    }
+  }, [faceMatches])
+
   const handleCapture = (base64: string) => setImage(base64)
 
   return (
     <Box $alignItems="center">
+      {indicatorEl && (
+        <LoaderContainer {...containerProps}>{indicatorEl}</LoaderContainer>
+      )}
       <Webcam onCapture={handleCapture} />
+      <Box $margin={{ top: 'large' }}>
+        {JSON.stringify(studentInfo, null, 2)}
+      </Box>
     </Box>
   )
 }
