5fe1b55604eb1e2898c790803c6ed7e354b35848
mannvender <mannvender001@gmail.com>
2021-04-12 00:15:01 +0530

    created Paragraph, updaated Heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const StyledHeading = styled.h1<{ color?: string }>`
   color: ${({ theme, color }) => color || theme.color['accent-2']};
   margin-top: 0;
+  margin-bottom: ${({ theme }) => theme.edgeSize.medium};
 `
 interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
   color?: string
