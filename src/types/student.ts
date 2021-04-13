e99f143c9d1973fb4a31423155f540f4aa46ef0e
mannvender <mannvender001@gmail.com>
2021-04-13 13:22:52 +0530

    updated student interface

--- src/types/student.ts
+++ src/types/student.ts
@@ -1,22 +1,22 @@
 export interface Student {
   id: string | number
   faceId?: string
-  image: any
-  admissionNo: string
+  image?: any
+  admissionNo?: string
   name: string
-  fatherName: string
+  fatherName?: string
   mobileNo: number
   gender: 'Male' | 'Female' | 'Other'
-  identificationDoc: 'Aadhar' | 'Driving Licence' | 'Passport' | 'Other'
+  identificationDoc?: 'Aadhar' | 'Driving Licence' | 'Passport' | 'Other'
   identificationDocNo: string
-  address: string
-  territory: 'Urban' | 'Rural'
-  router: string
+  address?: string
+  territory?: 'Urban' | 'Rural'
+  router?: string
   lockerNo?: string
   reservedSeat?: string
   dateOfJoining: Date
   validUpto: Date
-  timeSlotIn: Date
-  timeSlotOut: Date
-  feePaid: boolean
+  timeSlotIn?: string
+  timeSlotOut?: string
+  feePaid?: boolean
 }
